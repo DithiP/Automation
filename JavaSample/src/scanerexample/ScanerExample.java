3eef70d957d4c3f8ff07fba0b46bb62077f11dc2
HP <HP@LAPTOP-KQRS1J5O>
2024-08-24 13:04:09 +0530

    new code

--- JavaSample/src/scanerexample/ScanerExample.java
+++ JavaSample/src/scanerexample/ScanerExample.java
@@ -9,12 +9,13 @@ public class ScanerExample {
 		Scanner obj=new Scanner(System.in);
 		System.out.println("Enter your name");
 		String s=obj.nextLine();
-	
+	System.out.println(s);
 		System.out.println("Enter an intiger value");
 	int a=obj.nextInt();
-//System.out.println(a);
+System.out.println(a);
 	System.out.println("Enter a charactor");
-	char ch=obj.next().charAt(0);
+	char ch=obj.next().charAt(2);
+	System.out.println(ch);
 	
 
 	}
